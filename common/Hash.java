f264928de2803800ff5cc2d20bf6de46a0ab1855
Mitch Myburgh <mitchmyburgh@gmail.com>
2016-04-04 20:15:07 +0200

    added print statements

--- common/Hash.java
+++ common/Hash.java
@@ -3,11 +3,16 @@ package common;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+* Modified from http://stackoverflow.com/questions/415953/how-can-i-generate-an-md5-hash
+*/
 public class Hash {
 
 	public static String hash(String plainText)
 	{
-		String hash="";	
+		System.out.println("=== Hash.hash() called ===");
+		System.out.println("plaintext: "+plainText);
+		String hash="";
 		String data = plainText;
 		MessageDigest messageDigest;
 		try {
@@ -23,11 +28,13 @@ public class Hash {
 			// TODO Auto-generated catch block
 			exception.printStackTrace();
 		}
+		System.out.println("hash: "+hash);
+		System.out.println("=== Hash.hash() finished ===");
 		return hash;
 	}
 	public static void main(String[] args){
-		String a=Hash.hash("Hello World I am tumelobjdfdffgjhiofgfgjiogjiofgjio");
+		String a=Hash.hash("Hello World I am Tumelo");
 		System.out.println(a);
-		}
+	}
 
 }
--- common/Zipfile.java
+++ common/Zipfile.java
@@ -9,79 +9,89 @@ import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 import java.io.UnsupportedEncodingException;
 
+/**
+ * Modified from http://stackoverflow.com/questions/16351668/compression-and-decompression-of-string-data-in-java
+ */
 public class Zipfile {
 
-    public static byte[] compress(final String str) {
-        if ((str == null) || (str.length() == 0)) {
-            return null;
-        }
-        ByteArrayOutputStream obj = new ByteArrayOutputStream();
-        GZIPOutputStream gzip = null;
-        try {
-            gzip = new GZIPOutputStream(obj);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        
-        try {
-           gzip.write(str.getBytes("UTF-8"));
-        } catch (UnsupportedEncodingException e){
-            e.printStackTrace();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        try {
-            gzip.close();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        
-        return obj.toByteArray();
+  public static byte[] compress(final String str) {
+    System.out.println("=== Zipfile.compress() called ===");
+    System.out.println("plaintext: "+str);
+    if ((str == null) || (str.length() == 0)) {
+      return null;
+    }
+    ByteArrayOutputStream obj = new ByteArrayOutputStream();
+    GZIPOutputStream gzip = null;
+    try {
+      gzip = new GZIPOutputStream(obj);
+    } catch (IOException e){
+      e.printStackTrace();
     }
 
-    public static String decompress(final byte[] compressed) {
-        String outStr = "";
-        if ((compressed == null) || (compressed.length == 0)) {
-            return "";
-        }
-        if (isCompressed(compressed)) {
-            GZIPInputStream gis = null;
-            try {
-                gis = new GZIPInputStream(new ByteArrayInputStream(compressed));
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-            BufferedReader bufferedReader = null;
-            try {
-                bufferedReader = new BufferedReader(new InputStreamReader(gis, "UTF-8"));
-            } catch (UnsupportedEncodingException e){
-                e.printStackTrace();
-            }
-            
-
-            String line;
-            try {
-                while ((line = bufferedReader.readLine()) != null) {
-                    outStr += line;
-                }
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-        } else {
-            outStr = new String(compressed);
-        }
-        return outStr;
+    try {
+      gzip.write(str.getBytes("UTF-8"));
+    } catch (UnsupportedEncodingException e){
+      e.printStackTrace();
+    }catch (IOException e){
+      e.printStackTrace();
     }
-
-    public static boolean isCompressed(final byte[] compressed) {
-        return (compressed[0] == (byte) (GZIPInputStream.GZIP_MAGIC)) && (compressed[1] == (byte) (GZIPInputStream.GZIP_MAGIC >> 8));
+    try {
+      gzip.close();
+    } catch (IOException e){
+      e.printStackTrace();
     }
-    public static void main(String[] args){
-		byte[] a=null;
-		String d="";
-		 a =compress("Hello world");
-		 d=decompress(a);
-		 System.out.println(a);
-		System.out.println(d);	
-		}
+    System.out.println("zipped text: "+obj.toByteArray());
+    System.out.println("=== Zipfile.compress() finished ===");
+    return obj.toByteArray();
+  }
+
+  public static String decompress(final byte[] compressed) {
+    System.out.println("=== Zipfile.decompress() called ===");
+    System.out.println("zipped file: "+compressed);
+    String outStr = "";
+    if ((compressed == null) || (compressed.length == 0)) {
+      return "";
+    }
+    if (isCompressed(compressed)) {
+      GZIPInputStream gis = null;
+      try {
+        gis = new GZIPInputStream(new ByteArrayInputStream(compressed));
+      } catch (IOException e){
+        e.printStackTrace();
+      }
+      BufferedReader bufferedReader = null;
+      try {
+        bufferedReader = new BufferedReader(new InputStreamReader(gis, "UTF-8"));
+      } catch (UnsupportedEncodingException e){
+        e.printStackTrace();
+      }
+
+
+      String line;
+      try {
+        while ((line = bufferedReader.readLine()) != null) {
+          outStr += line;
+        }
+      } catch (IOException e){
+        e.printStackTrace();
+      }
+    } else {
+      outStr = new String(compressed);
+    }
+    System.out.println("plaintext: "+outStr);
+    System.out.println("=== Zipfile.decompress() finished ===");
+    return outStr;
+  }
+
+  public static boolean isCompressed(final byte[] compressed) {
+    return (compressed[0] == (byte) (GZIPInputStream.GZIP_MAGIC)) && (compressed[1] == (byte) (GZIPInputStream.GZIP_MAGIC >> 8));
+  }
+  public static void main(String[] args){
+    byte[] a=null;
+    String d="";
+    a =compress("Hello world");
+    d=decompress(a);
+    //System.out.println(a);
+    //System.out.println(d);
+  }
 }
