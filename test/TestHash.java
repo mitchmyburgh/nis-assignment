6a04716bdbd508b828569a032ff230bb9dfa380f
Mitch Myburgh <mitchmyburgh@gmail.com>
2016-04-05 07:28:43 +0200

    fin

--- test/TestHash.java
+++ test/TestHash.java
@@ -3,12 +3,26 @@ package test;
 import org.junit.Test;
 import org.junit.Ignore;
 import static org.junit.Assert.assertEquals;
+import java.io.PrintStream;
+import java.io.OutputStream;
+
+import common.Hash;
 
 public class TestHash {
 
    @Test
-   public void testPrintMessage() {
-      //System.out.println("Inside testPrintMessage()");
-      //assertEquals(message, messageUtil.printMessage());
+   public void testHash() {
+      System.out.println("Testing common.Hash.hash()");
+      String expectedOutput = "b10a8db164e0754105b7a99be72e3fe5";
+      //suppress output via http://stackoverflow.com/questions/8363493/hiding-system-out-print-calls-of-a-class
+      PrintStream originalStream = System.out;
+      PrintStream dummyStream    = new PrintStream(new OutputStream(){
+          public void write(int b) {
+          }
+      });
+      System.setOut(dummyStream);
+      String output = Hash.hash("Hello World");
+      System.setOut(originalStream);
+      assertEquals(output, expectedOutput);
    }
 }
--- test/TestZipfile.java
+++ test/TestZipfile.java
@@ -3,12 +3,42 @@ package test;
 import org.junit.Test;
 import org.junit.Ignore;
 import static org.junit.Assert.assertEquals;
+import java.io.PrintStream;
+import java.io.OutputStream;
+
+import common.Zipfile;
 
 public class TestZipfile {
 
    @Test
-   public void testPrintMessage() {
-      //System.out.println("Inside testPrintMessage()");
-      //assertEquals(message, messageUtil.printMessage());
+   public void testCompress() {
+      System.out.println("Testing common.Zipfile.compress()");
+      String expectedOutput = "H4sIAAAAAAAAAPNIzcnJVwjPL8pJAQBWsRdKCwAAAA==";
+      //suppress output via http://stackoverflow.com/questions/8363493/hiding-system-out-print-calls-of-a-class
+      PrintStream originalStream = System.out;
+      PrintStream dummyStream    = new PrintStream(new OutputStream(){
+          public void write(int b) {
+          }
+      });
+      System.setOut(dummyStream);
+      String output =  new String(org.apache.commons.codec.binary.Base64.encodeBase64(Zipfile.compress("Hello World")));
+      System.setOut(originalStream);
+      assertEquals(output, expectedOutput);
+   }
+
+    @Test
+   public void testDecompress() {
+      System.out.println("Testing common.Zipfile.decompress()");
+      String expectedOutput = "Hello World";
+      //suppress output via http://stackoverflow.com/questions/8363493/hiding-system-out-print-calls-of-a-class
+      PrintStream originalStream = System.out;
+      PrintStream dummyStream    = new PrintStream(new OutputStream(){
+          public void write(int b) {
+          }
+      });
+      System.setOut(dummyStream);
+      String output =  Zipfile.decompress(org.apache.commons.codec.binary.Base64.decodeBase64("H4sIAAAAAAAAAPNIzcnJVwjPL8pJAQBWsRdKCwAAAA==".getBytes()));
+      System.setOut(originalStream);
+      assertEquals(output, expectedOutput);
    }
 }
